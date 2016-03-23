a74278140ec9ee9fc551a2984dbc2c7ca29dac55
Macrovve <macrovve@gmail.com>
2016-03-23 19:51:16 +0800

    feat: add ex1.1.8

---
+++ 1.Fundamentals/1.1 Programming Model/ex_1_1_8.java
@@ -0,0 +1,7 @@
+public class ex_1_1_8 {
+    static public void main(String[] args) {
+        System.out.println('b');
+        System.out.println('b' + 'c');        //197 ASCII相加
+        System.out.println((char) ('a' + 4)); //e 也是ASCII相加，但是（char）强制转换了
+    }
+}
