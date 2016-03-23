35d4ceb90605cc917b52e86d73bb7641d6036b44
Macrovve <macrovve@gmail.com>
2016-03-23 19:44:12 +0800

    feat: add ex1.1.2

---
+++ 1.Fundamentals/1.1 Programming Model/ex_1_1_2.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class ex_1_1_2 {
+    static public void main(String[] args) {
+        StdOut.println((1 + 2.236) / 2);    //double
+        StdOut.println(1 + 2 + 3 + 4.0);      //double
+        StdOut.println(4.1 >= 4);         //bool
+        StdOut.println(1 + 2 + "3");        //string
+
+    }
+}
